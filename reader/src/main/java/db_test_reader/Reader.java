bd5baea78931c4ead25027854a72216da255517e
Peter Bona <peter.bona@msci.com>
2023-01-22 15:31:34 +0100

    First code

---
+++ reader/src/main/java/db_test_reader/Reader.java
@@ -0,0 +1,13 @@
+package db_test_reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reader {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.main(args);
+	}
+
+}
---
+++ writer/src/main/java/db_test_writer/Writer.java
@@ -0,0 +1,13 @@
+package db_test_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Writer {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.main(args);
+	}
+
+}
